5fb3ed4cdd8cc6c4461fc6964f7f80e13d126b16
LoungeAssistnat <loungeassistant@bi.tk>
2014-09-08 13:04:19 +0200

    fix

--- Lounge_Assistant.user.js
+++ Lounge_Assistant.user.js
@@ -4,13 +4,13 @@
 // @name        Lounge Assistant
 // @namespace   csgolounge.com/*
 // @include     http://csgolounge.com/*
-// @version     1.3.5
+// @version     1.3.6
 // @grant       GM_xmlhttpRequest
 // @grant       GM_addStyle
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_getResourceText
-// @resource css https://raw.githubusercontent.com/LoungeAssistant/Lounge-Assistant/master/style.css#1.3.5
+// @resource css https://raw.githubusercontent.com/LoungeAssistant/Lounge-Assistant/master/style.css#1.3.6
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 
 // ==/UserScript==
@@ -314,10 +314,13 @@ function winLoss()
 
 
 function addInventoryLink(){
+    if ($(".profilesmallheader>a").length < 1)
+	return;
     var steamid = $(".profilesmallheader>a").attr("href").match(/\d+/)[0];
     $(".profilesmallheader").append($("<a>").attr("href", "http://steamcommunity.com/profiles/" + steamid + "/inventory").text("Inventory"));
 
 }
+
 
 
 addInventoryLink();
@@ -327,6 +330,7 @@ displayBotStatus();
 isUpToDate();
 if (isLogged)
     winLoss();
+
 
 
 $(".match").on('mouseenter', function (){
